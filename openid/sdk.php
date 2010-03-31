3acd67284fb236fffe1d1b0e3d228dbe0fb01b09
erik <erikeldridge@yahoo.com>
2010-03-30 23:46:23 -0700

    minor edits to sdk, & now passing all tests in test.php

--- openid/sdk.php
+++ openid/sdk.php
@@ -4,7 +4,7 @@ function yql( $query )
     $params = array(
         'q' => $query,
         'callback' => '',
-        'diagnostics' => 'false',
+        'diagnostics' => 'true',
         'format' => 'json',
         'debug' => 'true'
     );
@@ -16,7 +16,7 @@ function yql( $query )
 function normalize_id( $id )
 {
     $table_uri = 'http://github.com/erikeldridge/yql-tables/raw/master/openid/openid.normalize.xml';
-    $query = "use '$table_uri' as table; select * from table where id='$id'"
+    $query = "use '$table_uri' as table; select * from table where id='$id'";
     $results = yql( $query );
     if( $results->error ){
         throw new Exception( 'error response: '.$results->error );
@@ -26,7 +26,7 @@ function normalize_id( $id )
 function perform_yadis( $uri )
 {
     $table_uri = 'http://github.com/erikeldridge/yql-tables/raw/master/openid/openid.yadis.xml';
-    $query = "use '$table_uri' as table; select * from table where uri='$uri'"
+    $query = "use '$table_uri' as table; select * from table where uri='$uri'";
     $results = yql( $query );
     if( $results->error ){
         throw new Exception( 'error response: '.$results->error );
@@ -36,7 +36,7 @@ function perform_yadis( $uri )
 function fetch_association( $op_uri )
 {
     $table_uri = 'http://github.com/erikeldridge/yql-tables/raw/master/openid/openid.assoc.xml';
-    $query = "use '$table_uri' as table; select * from table where uri='$op_uri'"
+    $query = "use '$table_uri' as table; select * from table where uri='$op_uri'";
     $results = yql( $query );
     if( $results->error ){
         throw new Exception( 'error response: '.$results->error );
@@ -46,9 +46,9 @@ function fetch_association( $op_uri )
 function fetch_auth_url( $id, $return_to, $assoc_handle = null, $ax_json = null )
 {
     $table_uri = 'http://github.com/erikeldridge/yql-tables/raw/master/openid/openid.xml';
-    $query = "use '$table_uri' as table; select * from table where id='$id' and return_to='$return_to'"
+    $query = "use '$table_uri' as table; select * from table where id='$id' and return_to='$return_to'";
     if( $assoc_handle ){
-        $query .= " and assocHandle='$assoc_handle'";
+        $query .= " and assoc_handle='$assoc_handle'";
     }
     if( $ax_fields ){
         $query .= " and axJson='$ax_json'";
@@ -62,7 +62,7 @@ function fetch_auth_url( $id, $return_to, $assoc_handle = null, $ax_json = null 
 function verify_assertion( $local_uri, $assert_json, $assoc_json = null, $nonce_store_uri = null )
 {
     $table_uri = 'http://github.com/erikeldridge/yql-tables/raw/master/openid/openid.verify.xml';
-    $query = "use '$table_uri' as table; select * from table where localUri='$local_uri' and assertJson='$assert_json'"
+    $query = "use '$table_uri' as table; select * from table where localUri='$local_uri' and assertJson='$assert_json'";
     if( $assoc_json ){
         $query .= " and assocJson='$assoc_json'";
     }
--- openid/test.php
+++ openid/test.php
@@ -1,34 +1,124 @@
 <?php
-function test_fetch_auth_url( $id, $return_to='http://example.com' ) {
-    $table_uri = 'http://github.com/erikeldridge/yql-tables/raw/master/openid/openid.xml';
-    $params = array(
-        'q' => "use '$table_uri' as table; select * from table where id='$id' and return_to='$return_to'",
-        'callback' => '',
-        'diagnostics' => 'false',
-        'format' => 'json',
-        'debug' => 'true'
-    );
-    $yql_uri = "http://query.yahooapis.com/v1/public/yql?".http_build_query( $params );
-    $json = file_get_contents( $yql_uri );
-    $data = json_decode( $json );
-    if( !$data->query->results->success ){
-        throw new Exception('error response');
-    }
-    $parsed_url = parse_url( $data->query->results->success );
+require 'sdk.php';
+
+function validate_structure( $url )
+{
+    $parsed_url = parse_url( $url );
     parse_str( $parsed_url['query'], $parsed_query );
+    
+    //default
     $properties = array( 'openid.ns', 'openid.realm', 'openid.mode', 'openid.return_to', 'openid.identity', 'openid.claimed_id' );
+    
+    //assoc
+    if($assoc_handle){
+        $properties[] = 'openid.assoc_handle';
+    }
+    
+    //ax
+    if($ax_params){
+        foreach( $ax_params as $param ){
+            $properties[] = 'openid.ax.type.'.$param['alias'];
+        }
+        $properties[] = 'openid.ax.mode';
+        $properties[] = 'openid.ax.required';
+    }
+    
+    //to minimize complexity for now, only check if field is defined
     foreach( $properties as $property ){
         if( !array_key_exists( str_replace( '.', '_', $property ), $parsed_query ) ){
-            throw new Exception( 'property missing: '.var_export( $parsed_query, true ) );
+            throw new Exception( 'property missing: '.var_export( $property, true ) );
         }
     }
+    
 }
 
+//yahoo setup
+$yahoo_assoc = fetch_association( 'https://open.login.yahooapis.com/openid/op/auth' );
+// $google_assoc = fetch_association( '' );
+$ax_params = array(
+    array(
+        'schema' => '',
+        'alias' => 'email',
+        'required' => 'false'
+    ),
+    array(
+        'schema' => '',
+        'alias' => 'fullname',
+        'required' => 'false'
+    )
+);
+
+//yahoo w/o assoc handle, ax
 try {
+    $test = 'yahoo w/o assoc handle, ax';
     $openid = 'yahoo.com';
-    test_fetch_auth_url( $openid, 'http://test.erikeldridge.com/yql/openid/' );
-    printf('pass: %s', $openid );
+    $return_to = 'http://test.erikeldridge.com/yql/openid/';
+    $url = fetch_auth_url( $openid, $return_to );
+    validate_structure( $url );
+    printf('pass: %s<br/>', $test );
 } catch (Exception $e) {
-   printf('<pre>%s</pre>', print_r( $e, true ) );
+   printf("<p>fail:<br/><pre>%s</pre></p>", print_r( "$test\n$e", true ) );
 }
+
+//yahoo w/ assoc handle
+try {
+    $test = 'yahoo w/ assoc handle';
+    $openid = 'yahoo.com';
+    $return_to = 'http://test.erikeldridge.com/yql/openid/';
+    $url = fetch_auth_url( $openid, $return_to, $yahoo_assoc->assoc_handle );
+    validate_structure( $url );
+    printf('pass: %s<br/>', $test );
+} catch (Exception $e) {
+   printf("<p>fail:<br/><pre>%s</pre></p>", print_r( "$test\n$e", true ) );
+}
+
+//yahoo w/ ax
+try {
+    $test = 'yahoo w/ ax';
+    $openid = 'yahoo.com';
+    $return_to = 'http://test.erikeldridge.com/yql/openid/';
+    $url = fetch_auth_url( $openid, $return_to, null, json_encode( $ax_params ) );
+    validate_structure( $url );
+    printf('pass: %s<br/>', $test );
+} catch (Exception $e) {
+   printf("<p>fail:<br/><pre>%s</pre></p>", print_r( "$test\n$e", true ) );
+}
+
+//yahoo w/ assoc handle, ax
+try {
+    $test = 'yahoo w/ assoc handle, ax';
+    $openid = 'yahoo.com';
+    $return_to = 'http://test.erikeldridge.com/yql/openid/';
+    $url = fetch_auth_url( $openid, $return_to, $yahoo_assoc->assoc_handle, json_encode( $ax_params ) );
+    validate_structure( $url );
+    printf('pass: %s<br/>', $test );
+} catch (Exception $e) {
+   printf("<p>fail:<br/><pre>%s</pre></p>", print_r( "$test\n$e", true ) );
+}
+
+//test valid base url
+//yahoo
+$test = 'test valid base url for yahoo';
+$openid = 'yahoo.com';
+$return_to = 'http://test.erikeldridge.com/yql/openid/';
+$url = fetch_auth_url( $openid, $return_to );
+$base_url = substr( $url, 0, strpos( $url, '?' ) );
+if( 'https://open.login.yahooapis.com/openid/op/auth' == $base_url ){
+    printf('pass: %s<br/>', $test );
+} else {
+   printf("<p>fail:<br/><pre>%s</pre></p>", print_r( "$base_url\n$e", true ) );
+}
+
+//google
+$test = 'test valid base url for google';
+$openid = 'yahoo.com';
+$return_to = 'http://test.erikeldridge.com/yql/openid/';
+$url = fetch_auth_url( $openid, $return_to );
+$base_url = substr( $url, 0, strpos( $url, '?' ) );
+if( 'https://open.login.yahooapis.com/openid/op/auth' == $base_url ){
+    printf('pass: %s<br/>', $test );
+} else {
+   printf("<p>fail:<br/><pre>%s</pre></p>", print_r( "$base_url\n$e", true ) );
+}
+
 ?>
