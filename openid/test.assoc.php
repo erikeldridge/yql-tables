89212fa0be9a0f3f02ae3127e243380277f9e99c
erik <erikeldridge@yahoo.com>
2010-03-30 23:55:01 -0700

    now printing output in a more consistent way

--- openid/test.assoc.php
+++ openid/test.assoc.php
@@ -1,32 +1,34 @@
 <?php
-function test_assoc_endpoint($op_uri) {
-    $table_uri = 'http://github.com/erikeldridge/yql-tables/raw/master/openid/openid.assoc.xml';
-    $op_uri = 'https://open.login.yahooapis.com/openid/op/auth';
-    $params = array(
-        'q' => "use '$table_uri' as table; select * from table where uri='$op_uri'",
-        'callback' => '',
-        'diagnostics' => 'false',
-        'format' => 'json',
-        'debug' => 'true'
-    );
-    $yql_uri = "http://query.yahooapis.com/v1/public/yql?".http_build_query( $params );
-    $json = file_get_contents( $yql_uri );
-    $data = json_decode( $json );
-    if(!$data->query->results->success){
-        throw new Exception('error response');
-    }
+require 'sdk.php';
+
+function validate_assoc( $assoc ) {
     $properties = array('ns', 'assoc_handle', 'session_type', 'assoc_type', 'expires_in');
     foreach( $properties as $property ){
-        if( !property_exists( $data->query->results->success, $property ) ){
+        if( !property_exists( $assoc, $property ) ){
             throw new Exception('property missing');
         }
     }
 }
+
+//yahoo
+$test = 'yahoo';
+$op_uri = 'https://open.login.yahooapis.com/openid/op/auth';
 try {
-    $op_uri = 'https://open.login.yahooapis.com/openid/op/auth';
-    test_assoc_endpoint( $op_uri );
-    printf('pass: %s', $op_uri );
+    $assoc = fetch_association( $op_uri );
+    validate_assoc( $assoc );
+    printf('pass: %s<br/>', $test );
 } catch (Exception $e) {
-   printf('<pre>%s</pre>', print_r( $e->message, true ) );
+   printf("<p>fail $test:<br/><pre>%s</pre></p>", print_r( $e->getMessage(), true ) );
+}
+
+//google
+$test = 'google';
+$op_uri = '';
+try {
+    $assoc = fetch_association( $op_uri );
+    validate_assoc( $assoc );
+    printf('pass: %s<br/>', $test );
+} catch (Exception $e) {
+   printf("<p>fail $test:<br/><pre>%s</pre></p>", print_r( $e->getMessage(), true ) );
 }
 ?>
